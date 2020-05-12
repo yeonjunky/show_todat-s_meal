c6d5f5da3c542ea29cbb408c70cd87a7963cc0c1
yeonjunky <yeonjunky@gmail.com>
2020-05-12 12:11:46 +0900

    .

---
+++ venv/templates/index.js
@@ -0,0 +1,13 @@
+// function readTxtFile(file){
+//     var http = new XMLHttpRequest();
+//     http.open("GET", file, false);
+//     http.onreadystatechange = function ()
+//     {
+//         if(http.readyState == 4){
+//             if(http.status == 200){
+//                 var alltext = http.responseText;
+//                 console.log(alltext)
+//             }
+//         }
+//     }
+// }
