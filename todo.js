caa75a72ebf87d002cd63416e38f41e9e88365d8
yeonjunky <yeonjunky@gmail.com>
2020-06-06 03:52:09 +0900

    update todo

--- todo.js
+++ todo.js
@@ -1,34 +1,68 @@
 const toDoform = document.querySelector(".js_toDoForm"),
-    toDoinput = toDoform.querySelector("#todoinput"),
+    toDoInput = toDoform.querySelector("input"),
     toDoList = document.querySelector(".js_toDoList");
 
-const TODOS_Ls = 'toDos';
+const TODOS_LS = 'toDos';
 
-function displayToDo(text) {
-    const il = document.createElement("li");
-    const delBtn = document.createElement("button");
-    delBtn.value = "x"
-    const span = docuemnt.createElement("span");
-    span.innerText = text;
+let toDos = [];
+
+function saveTodos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
-function handleSumbit(event) {
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+      return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveTodos();
+  }
+
+function displayToDo(text) {
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText = "X";
+    delBtn.addEventListener("click", deleteToDo)
+    span.innerText = text;
+    if(text !== ''){
+        li.appendChild(span);
+        li.appendChild(delBtn);
+        li.id = newId;
+        toDoList.appendChild(li);
+        const todoObj = {
+            text: text,
+            id: newId
+        };
+        toDos.push(todoObj);
+        saveTodos()
+    }
+}
+
+function handleSubmit(event) {
     event.preventDefault();
-    const currnetValue = toDoinput.value;
-    displayToDo(currnetValue);
-    toDoinput.value = "";
+    const currentValue = toDoInput.value;
+    displayToDo(currentValue);
+    toDoInput.value = "";
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_Ls);
-    if(toDos === null){
-
+    const loadedtoDos = localStorage.getItem(TODOS_LS);
+    if(loadedtoDos !== null){
+        const paresdToDos = JSON.parse(loadedtoDos);
+        paresdToDos.forEach(function(todo) {
+            displayToDo(todo.text);
+        });
     }
 }
 
 function init() {
-    loadToDos()
-    toDoform.addEventListener("sumbit", handleSumbit)
+    loadToDos();
+    toDoform.addEventListener("submit", handleSubmit);
 }
 
-init()
+init();
